d8ff78ccd70d709171fbfb4ad8f46758622e62ca
arfaidrees <arfaidrees@users.noreply.github.com>
2024-08-17 10:25:21 +0000

    deploy: ed43b90e5a96c73d8854b50912c73135dece8b12

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "950222192"
+    serviceWorkerVersion: "3154549807"
   }
 });
